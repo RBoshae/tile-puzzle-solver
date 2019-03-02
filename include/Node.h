b23e2546b310fef61552dbc6307ed3235a517a74
rboshae <rwboshae@gmail.com>
2019-03-02 03:10:41 -0800

    Refactored Node class

--- include/Node.h
+++ include/Node.h
@@ -10,40 +10,34 @@
 #include <string>
 #include <vector>
 
-#ifndef NODE_H
-#define NODE_H
+#ifndef __NODE_H__
+#define __NODE_H__
 class Node
 {
 public:
-	Node();
+	Node(Board const &_board, int _heuristicCost);
+  Node(Board const &_board, Node const &_parent, string const _moveApplied);
 	~Node();
 
-	int 	getCost();
-	void 	setParentNode(Node *passed_parent);
-	Node* getParentNode();
-	Node 	previous_state(Node current_state);
+  // void 	setParent(Node *passed_parent);
+  Node* getParentNode();
+
+  int 	getPathCost();
+  int   getHeuristicCost();
+  int 	getTotalCost();
 
 	Board getBoard();
-	void 	printNodeBoard();
 
-	int 	getNodeDepth();
-
-	int 	getFofN();
-
-	string  move_applied;  	//Keeps track of move applied to node
-	vector<Board> board_history;
-
-	int h_of_n;            	//Contains value of h(n). Will create setter for this and make private.
 
 private:
-	int f_of_n;			   			//Contains value of f(n)
-	int path_cost;         	//Path Cost of Node
-	int node_depth;		   		//Keeps track of node depth. Later used to compute g(n)
-	int manhattan_distance;
-	int misplaced_tile;
-	Node* parent;          	//Points to parent node in tree.
-	Board node_board;      	// Contains copy of Board
+  Board   m_board;      	        // Contains copy of Board
+  Node*   m_parent;          	      // Pointer to parent node.
+  string  m_moveDesc;             // Description of move applied to parent to reach current state.
+  int     m_pathCost;             // Also known as g(n).
+  int     m_heuristicCost;        // Also known as h(n).
+	int     m_totalCost;		   	    // Contains value of f(n)
+
 
 };
 
-#endif
+#endif  //__NODE_H__
--- src/Node.cpp
+++ src/Node.cpp
@@ -2,61 +2,53 @@
 
 
 
-Node::Node()
+Node::Node(Board const &_board, int _heuristicCost)
+:
+  m_board(_board),
+  m_parent(nullptr),
+  m_moveDesc(""),
+  m_pathCost(0),
+  m_heuristicCost(_heuristicCost),
+  m_totalCost(_heuristicCost)
 {
-	f_of_n = 0;
-	h_of_n = 0;
-	node_depth = 0;
-	path_cost = 0;
-	move_applied = "initial state";
-	parent = NULL;
 }
 
+Node::Node(Board const &_board, int _heuristicCost, Node const &_parent, string const _moveApplied)
+:
+  m_board(_board),
+  m_parent(_parent),
+  m_moveDesc(_moveApplied),
+  m_pathCost(_parent->getPathCost+1),
+  m_heuristicCost(_heuristicCost),
+  m_totalCost(m_pathCost+m_heuristicCost)
+{
+}
 
 Node::~Node()
 {
 }
 
-int Node::getCost()
-{
-	return f_of_n;
-}
-
-//set current node to point to parent. node_depth is modified here since we must know the depth of our parent to get the depth of the child
-void Node::setParentNode(Node *passed_parent)
-{
-	parent = passed_parent;
-	node_depth = passed_parent->getNodeDepth() + 1;
-	f_of_n = node_depth + h_of_n;
-	//board_history.push_back(passed_parent->getBoard());
-}
-
 Node* Node::getParentNode() {
-	return parent;
+	return m_parent;
 }
 
-Node Node::previous_state(Node current_state)
+int Node::getPathCost()
 {
-	// if (current_state.move_applied == "blank tile right") current_state.getBoard().move(3); return current_state;
-	return current_state; // TODO: Remove line.
+	return m_pathCost;
 }
+
+int Node::getHeuristicCost()
+{
+	return m_heuristicCost;
+}
+
+int Node::getTotalCost()
+{
+	return m_totalCost;
+}
+
 
 Board Node::getBoard()
 {
 	return node_board;
-}
-
-void Node::printNodeBoard()
-{
-	// node_board.printBoard();
-}
-
-int Node::getNodeDepth(){
-	return node_depth;
-}
-
-int Node::getFofN() {
-	//possibly redundant
-	f_of_n = node_depth + h_of_n;
-	return f_of_n;
 }
