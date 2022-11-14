6bfa3c7fbfe1a01bb5ee80133bfe4bfe79ff7111
Rick Boshae <rwboshae@gmail.com>
2022-11-13 21:14:26 -0800

    Add doxygen based comments. Update Puzzle ctor

--- include/Puzzle.h
+++ include/Puzzle.h
@@ -6,13 +6,43 @@
 
 #include <vector>
 
-class Puzzle
-{
-public:
-  Puzzle();
+//! Puzzle class.
+/* 
+Graphical representation of the puzzle. 
+  -----------------
+  |0,0|0,1|0,2|0,n|
+  |---+---+---+---|
+  |1,0|1,1|1,2|1,n|
+  |---+---+---+---|
+  |2,0|2,1|2,2|2,n|
+  |---+---+---+---|
+  |n,0|n,1|n,2|n,n|
+  -----------------
+*/
 
-  // Creates a Puzzle with a user-defined tile configuration.
+class Puzzle {
+public:
+  /*!
+      Initialize a square shaped Puzzle with n tiles. 
+
+      Note: if n+1 is not a perfect square then an error is thrown. 
+  */ 
+  Puzzle(unsigned int _tiles);
+
+  /*!
+      Initialze puzzle with n rows and m cols. 
+
+      Note: This method constructor allows rectangle shaped Puzzles.
+  */ 
+  Puzzle(int _rows, _cols);
+
+  /*!
+      Creates a Puzzle with a user-defined tile configuration.
+
+      Note: An error is thrown if the configuration is not a rectangle. 
+  */
   Puzzle(vector<vector<int>> _tile_configuration);
+  Puzzle(Puzzle _puzzle);
   ~Puzzle();
 
   vector<vector<int>> GetTileConfig();
@@ -30,8 +60,10 @@ public:
   bool operator ==(const Board &other) const;
 
 private:
-        vector<vector<int>> tile_configuration_;
-	pair<int> blank_tile_pos_;
+  vector<vector<int>> tile_configuration_;
+  pair<int> blank_tile_pos_;
+
+  bool Validate();
 };
 
 #endif
